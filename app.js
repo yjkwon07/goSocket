da5286f378bf16432625e65c96fee83e51bc2e8c
yjkwon07 <dudwns9073@nate.com>
2019-12-28 01:03:48 +0900

    middleware, router  settgin

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
+
+const webSocket = require('./socket');
+const indexRouter = require('./routes');
+const connect = require('./schemas');
+
+const app = express();
+connect();
+
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8005);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
+app.use(flash());
+
+// * color-hash는 그냥 익명 사용자를 컬러로 구분하기위한 패키지이다.
+app.use((req,_res,next)=>{
+  if(!req.session.color) {
+    const colorHash = new colorHash();
+    req.session.color = colorHash.hex(req.session);
+  }
+  next();
+});
+
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const server = app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
+
+// * Socket.IO에서도 미들웨어를 사용할 수 있습니다. 
+// use안에 (req,res,next)를 붙여주면 된다. 
+webSocket(server, app, sessionMiddleware);
