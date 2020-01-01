54cbae927898acfc0670c2243a00e9374f194f3c
yjkwon07 <dudwns9073@nate.com>
2020-01-01 21:42:54 +0900

    socket ip 확인 로그 삽입

--- socket.js
+++ socket.js
@@ -16,16 +16,22 @@ module.exports = (server, app, sessionMiddleWare) => {
   });
 
   room.on("connection", socket => {
-    console.log("room 네임스페이스에 접속");
+    const req = socket.request;
+    const ip = req.headers["x-forwarded-for"] || req.connection;
+
+    console.log("room 네임스페이스에 접속",ip, socket.id, req.ip);
     socket.on("disconnect", () => {
       console.log("room 네임스페이스 접속 해제");
     });
   });
 
   chat.on("connection", socket => {
-    console.log("chat 네임스페이스에 접속");
+
 
     const req = socket.request;
+    const ip = req.headers["x-forwarded-for"] || req.connection;
+    console.log("chat 네임스페이스에 접속", socket.id, req.ip);
+
     const {headers: { referer }} = req;
     const roomId = referer
             .split("/")[referer.split("/").length - 1]
@@ -44,12 +50,10 @@ module.exports = (server, app, sessionMiddleWare) => {
       const currentRoom = socket.adapter.rooms[roomId];
       const userCount = currentRoom ? currentRoom.length : 0;
       if (userCount === 0) {
-        // 직접 디비를 조작하지 말고 (socket통신은 socket 통신으로만),
-        // 라우터를 통해 조작하는게 좋다. (지저분함이 있을 수 있기 때문에)
         asxios
           .delete(`http://localhost:8015/room/${roomId}`)
           .then(() => {
-            console.log("방 제거 요청 성공");
+            console.log(`방: ${roomId} 제거 요청 성공`);
           })
           .catch(error => {
             console.log(error);
@@ -61,7 +65,7 @@ module.exports = (server, app, sessionMiddleWare) => {
         });
       }
     });
-
+    
   });
 
 };
